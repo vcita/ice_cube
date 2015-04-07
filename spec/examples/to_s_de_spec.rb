1e17bb8ae64434a204b7a9e64d67d9ee4eb2bcf5
Isaac Seymour <isaac.seymour@gmail.com>
2015-04-07 14:11:04 +0100

    Updates

--- spec/examples/to_s_de_spec.rb
+++ spec/examples/to_s_de_spec.rb
@@ -9,7 +9,7 @@ describe IceCube::Schedule, 'to_s' do
 
   it 'should represent its start time by default' do
     t0 = Time.local(2013, 2, 14)
-    IceCube::Schedule.new(t0).to_s.should == 'Februar 14, 2013'
+    IceCube::Schedule.new(t0).to_s.should == '14. Februar 2013'
   end
 
   it 'should have a useful base to_s representation for a secondly rule' do
@@ -29,7 +29,7 @@ describe IceCube::Schedule, 'to_s' do
 
   it 'should have a useful base to_s representation for a daily rule' do
     IceCube::Rule.daily.to_s.should == 'Täglich'
-    IceCube::Rule.daily(2).to_s.should == 'Jeden 2 Tag'
+    IceCube::Rule.daily(2).to_s.should == 'Alle 2 Tage'
   end
 
   it 'should have a useful base to_s representation for a weekly rule' do
@@ -49,9 +49,9 @@ describe IceCube::Schedule, 'to_s' do
 
   it 'should work with various sentence types properly' do
     IceCube::Rule.weekly.to_s.should == 'Wöchentlich'
-    IceCube::Rule.weekly.day(:monday).to_s.should == 'Wöchentlich an Montags'
-    IceCube::Rule.weekly.day(:monday, :tuesday).to_s.should == 'Wöchentlich an Montags und Diensags'
-    IceCube::Rule.weekly.day(:monday, :tuesday, :wednesday).to_s.should == 'Wöchentlich an Montags, Diensags, und Mittwochs'
+    IceCube::Rule.weekly.day(:monday).to_s.should == 'Wöchentlich: Montags'
+    IceCube::Rule.weekly.day(:monday, :tuesday).to_s.should == 'Wöchentlich: Montags und Dienstags'
+    IceCube::Rule.weekly.day(:monday, :tuesday, :wednesday).to_s.should == 'Wöchentlich: Montags, Dienstags, und Mittwochs'
   end
 
   it 'should show saturday and sunday as weekends' do
@@ -60,12 +60,12 @@ describe IceCube::Schedule, 'to_s' do
 
   it 'should not show saturday and sunday as weekends when other days are present also' do
     IceCube::Rule.weekly.day(:sunday, :monday, :saturday).to_s.should ==
-      'Wöchentlich an Sonntags, Montags, und Samstags'
+      'Wöchentlich: Sonntags, Montags, und Samstags'
   end
 
   it 'should reorganize days to be in order' do
     IceCube::Rule.weekly.day(:tuesday, :monday).to_s.should ==
-      'Wöchentlich an Montags und Diensags'
+      'Wöchentlich: Montags und Dienstags'
   end
 
   it 'should show weekdays as such' do
@@ -79,51 +79,51 @@ describe IceCube::Schedule, 'to_s' do
     IceCube::Rule.weekly.day(
       :sunday, :monday, :tuesday, :wednesday,
       :thursday, :friday
-    ).to_s.should == 'Wöchentlich an Sonntags, Montags, Diensags, Mittwochs, Donnerstags, und Freitags'
+    ).to_s.should == 'Wöchentlich: Sonntags, Montags, Dienstags, Mittwochs, Donnerstags, und Freitags'
   end
 
   it 'should show start time for an empty schedule' do
     schedule = IceCube::Schedule.new Time.local(2010, 3, 20)
-    schedule.to_s.should == "März 20, 2010"
+    schedule.to_s.should == "20. März 2010"
   end
 
   it 'should work with a single date' do
     schedule = IceCube::Schedule.new Time.local(2010, 3, 20)
     schedule.add_recurrence_time Time.local(2010, 3, 20)
-    schedule.to_s.should == "März 20, 2010"
+    schedule.to_s.should == "20. März 2010"
   end
 
   it 'should work with additional dates' do
     schedule = IceCube::Schedule.new Time.local(2010, 3, 20)
     schedule.add_recurrence_time Time.local(2010, 3, 20)
     schedule.add_recurrence_time Time.local(2010, 3, 21)
-    schedule.to_s.should == 'März 20, 2010 / März 21, 2010'
+    schedule.to_s.should == '20. März 2010 / 21. März 2010'
   end
 
   it 'should order dates that are out of order' do
     schedule = IceCube::Schedule.new Time.local(2010, 3, 20)
     schedule.add_recurrence_time Time.local(2010, 3, 19)
-    schedule.to_s.should == 'März 19, 2010 / März 20, 2010'
+    schedule.to_s.should == '19. März 2010 / 20. März 2010'
   end
 
   it 'should remove duplicated start time' do
     schedule = IceCube::Schedule.new t0 = Time.local(2010, 3, 20)
     schedule.add_recurrence_time t0
-    schedule.to_s.should == 'März 20, 2010'
+    schedule.to_s.should == '20. März 2010'
   end
 
   it 'should remove duplicate rtimes' do
     schedule = IceCube::Schedule.new Time.local(2010, 3, 19)
     schedule.add_recurrence_time Time.local(2010, 3, 20)
     schedule.add_recurrence_time Time.local(2010, 3, 20)
-    schedule.to_s.should == 'März 19, 2010 / März 20, 2010'
+    schedule.to_s.should == '19. März 2010 / 20. März 2010'
   end
 
   it 'should work with rules and dates' do
     schedule = IceCube::Schedule.new Time.local(2010, 3, 19)
     schedule.add_recurrence_time Time.local(2010, 3, 20)
     schedule.add_recurrence_rule IceCube::Rule.weekly
-    schedule.to_s.should == 'März 20, 2010 / Wöchentlich'
+    schedule.to_s.should == '20. März 2010 / Wöchentlich'
   end
 
   it 'should work with rules and times and exception times' do
@@ -132,7 +132,7 @@ describe IceCube::Schedule, 'to_s' do
     schedule.add_recurrence_time Time.local(2010, 3, 20)
     schedule.add_exception_time Time.local(2010, 3, 20) # ignored
     schedule.add_exception_time Time.local(2010, 3, 21)
-    schedule.to_s.should == 'Wöchentlich / nicht an März 20, 2010 / nicht an März 21, 2010'
+    schedule.to_s.should == 'Wöchentlich / nicht am 20. März 2010 / nicht am 21. März 2010'
   end
 
   it 'should work with a single rrule' do
@@ -148,12 +148,12 @@ describe IceCube::Schedule, 'to_s' do
 
   it 'should be able to say what months of the year something happens' do
     rule_str = IceCube::Rule.yearly.month_of_year(:june, :july).to_s
-    rule_str.should == 'Jährlich in Juni und Juli'
+    rule_str.should == 'Jährlich im Juni und Juli'
   end
 
   it 'should be able to say the second to last monday of the month' do
     rule_str = IceCube::Rule.monthly.day_of_week(:thursday => [-2]).to_s
-    rule_str.should == 'Monatlich am vorletzter Donnerstag'
+    rule_str.should == 'Monatlich am vorletzten Donnerstag'
   end
 
   it 'should join the first and last weekdays of the month' do
@@ -173,23 +173,23 @@ describe IceCube::Schedule, 'to_s' do
 
   it 'should be able to say what hour of the day something happens' do
     rule_str = IceCube::Rule.daily.hour_of_day(6, 12).to_s
-    rule_str.should == 'Täglich an den Stunden 6. und 12. des Tages'
+    rule_str.should == 'Täglich an den 6. und 12. Stunden des Tages'
   end
 
   it 'should be able to say what minute of an hour something happens - with special suffix minutes' do
     rule_str = IceCube::Rule.hourly.minute_of_hour(10, 11, 12, 13, 14, 15).to_s
-    rule_str.should == 'Stündlich an den Minuten 10., 11., 12., 13., 14., und 15. der Stunde'
+    rule_str.should == 'Stündlich an den 10., 11., 12., 13., 14., und 15. Minuten der Stunde'
   end
 
   it 'should be able to say what seconds of the minute something happens' do
     rule_str = IceCube::Rule.minutely.second_of_minute(10, 11).to_s
-    rule_str.should == 'Minütlich an Sekunde 10. und 11. der Minute'
+    rule_str.should == 'Minütlich an 10. und 11. Sekunde der Minute'
   end
 
   it 'should be able to reflect until dates' do
     schedule = IceCube::Schedule.new(Time.now)
     schedule.rrule IceCube::Rule.weekly.until(Time.local(2012, 2, 3))
-    schedule.to_s.should == 'Wöchentlich bis Februar 3, 2012'
+    schedule.to_s.should == 'Wöchentlich bis 3. Februar 2012'
   end
 
   it 'should be able to reflect count' do
